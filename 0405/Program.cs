851a4d503e5d6a4491fc877ceae5f1b931129481
DEV <dev@info.local>
2021-05-04 10:50:55 -0300

    exercicios aula hoje

---
+++ 0405/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _0405
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           for (int cinquenta = 0; cinquenta <= 50; cinquenta++)
+           {
+               Console.WriteLine("Contador: " + cinquenta);
+           }
+
+           for (int cem = 200; cem >= 100; cem--)
+           {
+               Console.WriteLine("Contador: " + cem);
+           }
+
+           for (double i = 0; i < 5; i = i + 0.1)
+           {
+               Console.WriteLine("Contador: " + i);
+           }
+
+        //    bool idadeTyler = false;
+
+        //     while (idadeTyler = false)
+        //     {
+                
+        //     }
+        }
+    }
+}
---
+++ Nadadores2.0/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Nadadores2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string classe = "";
+            string checar;
+
+            do
+            {
+            Console.WriteLine("Olá nadador, qual a sua idade?");
+            int idadeNadador = int.Parse(Console.ReadLine());
+
+                switch (idadeNadador)
+                {
+                    case 5:
+                    case 6:
+                    case 7:
+                    classe = "infantil A";
+                    
+                        break;
+                    case 8:
+                    case 9:
+                    case 10:
+                    classe = "infantil B";
+                    
+                        break;
+                    case 11:
+                    case 12:
+                    case 13:
+                    classe = "Juvenil A";
+                   
+                        break;
+                    case 14:
+                    case 15:
+                    case 16:
+                    case 17:
+                    classe = "Juvenil B";
+                    
+                        break;
+
+                    default:
+                    classe = "Infelizmente o indivíduo com essa idade não pode se inscrever na natação \n Digite uma idade valida \n";
+                        break;
+                }      
+
+            Console.WriteLine(classe);
+            Console.WriteLine("Você deseja checar mais uma idade? (sim/não)");
+            checar = Console.ReadLine().ToLower();
+
+            }while (checar == "sim");
+
+            Console.WriteLine("Tudo bem então, Tenha um bom dia caro usuário \n Fim do sistema");
+        }
+    }
+}
